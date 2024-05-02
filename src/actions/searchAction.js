1bad124c55ecf0e97be1ccb90eba49244e7ccc17
AlterShadow <thomas0xken@gmail.com>
2024-05-02 10:05:42 -0400

    update

--- src/actions/searchAction.js
+++ src/actions/searchAction.js
@@ -5,11 +5,11 @@ import {
 } from "./transactionAction";
 
 export const searchAction = async (value) => {
-  const blockResult = await getBlockByHash(value);
-  if (blockResult) return "block";
-
   const txHashResult = await getTransactionByHash(value);
   if (txHashResult) return "txHash";
+
+  const blockResult = await getBlockByHash(value);
+  if (blockResult) return "block";
 
   const txBlockResult = await GetTransactionsByBlockHash(value);
   if (txBlockResult) return "txBlock";
